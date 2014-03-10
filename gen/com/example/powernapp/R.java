028811d5196e9e591787712debdfc39934407523
Holzi146 <holzhauser.christoph@gmx.at>
2014-03-10 21:00:58 +0100

    10.03.2014: Update 2

--- gen/com/example/powernapp/R.java
+++ gen/com/example/powernapp/R.java
@@ -27,20 +27,23 @@ public final class R {
         public static final int pfeil=0x7f020003;
     }
     public static final class id {
-        public static final int about_us=0x7f09000e;
+        public static final int about_us=0x7f090011;
         public static final int btn_close=0x7f090000;
+        public static final int btn_playpause=0x7f090009;
         public static final int btn_search=0x7f090004;
         public static final int btn_start=0x7f090002;
+        public static final int btn_stop=0x7f09000a;
         public static final int cb_time=0x7f090001;
         public static final int frameLayout1=0x7f090008;
         public static final int iv_arrow=0x7f090006;
-        public static final int lv_songs=0x7f090009;
-        public static final int main=0x7f09000b;
-        public static final int musikplayer=0x7f09000c;
-        public static final int tipps=0x7f09000d;
+        public static final int lv_songs=0x7f09000b;
+        public static final int main=0x7f09000e;
+        public static final int musikplayer=0x7f09000f;
+        public static final int sb_volume=0x7f09000c;
+        public static final int tipps=0x7f090010;
         public static final int tp_time=0x7f090003;
         public static final int tv_klickmich=0x7f090005;
-        public static final int tv_text=0x7f09000a;
+        public static final int tv_text=0x7f09000d;
         public static final int videoView=0x7f090007;
     }
     public static final class layout {
--- src/com/example/powernapp/Musikplayer.java
+++ src/com/example/powernapp/Musikplayer.java
@@ -1,25 +1,23 @@
 package com.example.powernapp;
 
-import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.util.List;
-
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
-import android.database.Cursor;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class Musikplayer extends Activity {
 	
@@ -27,19 +25,39 @@ public class Musikplayer extends Activity {
 	ListView lv_songs;
 	MediaPlayer mediaPlayer;
 	ArrayAdapter<String> songList;
+	SeekBar sb_volume;
+	AudioManager audioManager;
+	Button btn_playpause, btn_stop;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_musikplayer);
-		actionBar = getActionBar();
-		actionBar.setTitle("Musikplayer");
+		getActionBar().setTitle("Musikplayer");
 		
 		mediaPlayer = new MediaPlayer();
-		
 		lv_songs = (ListView) findViewById(R.id.lv_songs);
-		
 		songList = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,0);
+		btn_playpause = (Button) findViewById(R.id.btn_playpause);
+		btn_stop = (Button) findViewById(R.id.btn_stop);
+		
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+		initSeekBar();
+		
+		/* Thread for updating the seekBar if the side-keys were pressed */
+		new Thread(new Runnable() { 
+	        @Override
+	        public void run() {
+	        	while (true) {
+	        		try {
+	                    sb_volume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)); 
+						Thread.sleep(100);						
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+	        	}
+	        }
+	    }).start();
 		
 		/* adding the songs to the list */
 		songList.add("powernApp - Song 1");
@@ -52,11 +70,43 @@ public class Musikplayer extends Activity {
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int which,
 					long arg3) {
-				playSongFromRaw(songList.getItem(which));		
+				playSongFromRaw(songList.getItem(which));
 			}
 	    });
 	}
 	
+	private void initSeekBar()
+    {
+        try
+        {
+        	sb_volume = (SeekBar)findViewById(R.id.sb_volume);
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            
+            sb_volume.setMax(audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC));
+            sb_volume.setProgress(audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+            
+            sb_volume.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+				
+				public void onStopTrackingTouch(SeekBar seekBar) {
+					/* Do stuff in here */
+				}
+				
+				public void onStartTrackingTouch(SeekBar seekBar) {
+					/* Do stuff in here */
+				}
+				
+				public void onProgressChanged(SeekBar seekBar, int progress,
+						boolean fromUser) {
+					audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                            progress, 0);					
+				}
+			});
+        }
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+    }
 	
 	private void playSongFromRaw(String file)  {
 
@@ -96,7 +146,6 @@ public class Musikplayer extends Activity {
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.musikplayer, menu);
 		return true;
 	}
